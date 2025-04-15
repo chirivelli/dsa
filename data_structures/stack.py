954184be86590c1875244ffc47283f04bb9e2df3
Sathwik <sathwikchirivelli@gmail.com>
2025-04-15 19:30:54 +0530

    Added MonoStack

---
+++ data_structures/stack.py
@@ -0,0 +1,24 @@
+class MonoStack:
+    def __init__(self):
+        self.stack = []
+
+    def append(self, val: int):
+        while self.stack and val > self.stack[-1]:
+            self.stack.pop()
+        self.stack.append(val)
+
+    def display(self):
+        print(self.stack)
+
+def main():
+    mono = MonoStack()
+    mono.append(5)
+    mono.append(3)
+    mono.append(6)
+    mono.append(7)
+    mono.append(1)
+    mono.display()
+
+
+if __name__ == '__main__':
+    main()
